91645b82ec0a0828c473234070731c0caaec0ce2
Hicham Abboud <bio2010hicham@gmail.com>
2022-11-13 05:06:53 -0500

    Update Navbar.js
    fixing navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 function Navbar() {
     const { theme, setTheme, systemTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => { setMounted(true)}, []);
 
@@ -42,9 +43,14 @@ function Navbar() {
                     </div>
 
                     <div className='flex space-x-4'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        {!isOpen ? 
+                        <svg onClick={() => {setIsOpen(true)}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg> :
+                        <svg onClick={() => {setIsOpen(false)}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
+                        }
 
                         {renderThemeChange()}
                     </div>
